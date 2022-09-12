2fe3f3fefe892fa87a95e22bfa2dae5b5a37524a
JesusRojasH <112908983+JesusRojasH@users.noreply.github.com>
2022-09-12 17:37:33 -0500

    Prueba de la Red Neuronal Simple sin Modificaciones.

---
+++ Red Sin Modificar/Prueba.py
@@ -0,0 +1,21 @@
+
+import network 
+import pickle 
+import mnist_loader 
+
+
+DTraining, DValidation, DTest = mnist_loader.load_data_wrapper()
+
+DTraining = list(DTraining)
+DTest = list(DTest)
+
+
+"""Prueba de la red neuronal básica sin modificar."""
+net = network.Network([784, 30, 10])
+net.SGD(DTraining, 30, 10, 3, test_data = DTest)
+
+File = open('red_prueba.pkl','wb')
+pickle.dump(net, File)
+File.close()
+exit()
+
